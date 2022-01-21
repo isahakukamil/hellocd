656c5062e88a83442bc4a14ecd0dbfc510c7529a
Issahaku Kamil <isahakukamil@gmail.com>
2022-01-21 18:17:52 +0000

    Fixing Code

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -1,5 +1,5 @@
 
-ckage com.example.hellocd;
+package com.example.hellocd;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class HellocdApplication {
 
 
